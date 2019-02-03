a295e86c5c2ba1cfd706c839113c8e34fe64ac61
Dan Bush <dan.bush@gmail.com>
2019-02-03 14:30:46 -0500

    cleanup

--- src/examples/simple.js
+++ src/examples/simple.js
@@ -1,6 +1,6 @@
 const drawbot = require('../drawbot')
 
-drawbot.move(5, 4) // vertical (from origin of 5,1)
+drawbot.move(5, 4) // vertical (from origin START_X,START_Y)
 drawbot.move(3, 6) // diagonal
 drawbot.move(7, 6) // horizontal
 
